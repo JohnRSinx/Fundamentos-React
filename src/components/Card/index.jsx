985dafc6df033461458f1229879567a6c4e10ed1
John Rodrigues <johnrodrigues97106@gmail.com>
2022-07-15 21:37:44 -0300

    state immutability

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+
+import "./styles.css";
+
+export function Card({name , time}) {
+ 
+  return (
+    <div className="card">
+      <strong>{name}</strong>
+      <small>{time}</small>
+    </div>
+  );
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,50 @@
+import React,{useState} from "react";
+import './styles.css';
+import { Card } from '../../components/Card';
+
+export function Home() {
+  const [studentName , setStudentName] = useState("");
+  const [students , setStudents] = useState([]);
+  
+  function handleAddStudent() {
+    const newStudent = {
+      name : studentName ,
+      time : new Date().toLocaleDateString("pt-BR" , {
+        hour:"2-digit",
+        minute:"2-digit",
+        second:"2-digit",
+      }), 
+    }
+
+    setStudents(prevState =>[...prevState , newStudent])
+    
+  };
+  
+
+  
+  return (
+    <div className="container">
+      <h1>Lista de Presença</h1>
+      <input 
+        type="text" 
+        placeholder="Digite o nome..."
+        onChange={e=>setStudentName(e.target.value)}
+      />
+
+      <button 
+      type="button"
+      onClick={handleAddStudent}
+      >Adicionar
+      </button>
+
+      { 
+        students.map(student =><Card name={student.name} time={student.time}/> )
+      }
+      
+      
+    </div>
+  );
+}
+
+
+
